fb58ac97c9636990ef1e9cef0e051594fe3e7f5f
JannisMeyer <80952891+JannisMeyer@users.noreply.github.com>
2023-10-20 20:01:28 +0200

    started implementing ble scanning functionality

--- app/src/main/java/com/example/bleapp/MainActivity.kt
+++ app/src/main/java/com/example/bleapp/MainActivity.kt
@@ -1,15 +1,44 @@
 package com.example.bleapp
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.bleapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
+
+    private val bluetoothAdapter : BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    private val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+    private var scanning = false
+    private val handler = Handler()
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private val permissions = arrayOf(Manifest.permission.BLUETOOTH_SCAN)
+    private val requestCode = 888
+
+    // Device scan callback.
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+
+        }
+    }
+
+    // Stops scanning after 10 seconds.
+    private val scanPeriod: Long = 10000
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,7 +48,46 @@ class MainActivity : AppCompatActivity() {
 
         //set OnClickListener for BLE-Button
         binding.button.setOnClickListener {
-            Toast.makeText(this, "BLE!", Toast.LENGTH_SHORT).show()
+            scanBleDevice()
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun scanBleDevice() {
+
+        if (!scanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                scanning = false
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.BLUETOOTH_SCAN
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(this, permissions, requestCode)
+                    return@postDelayed
+                }
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, scanPeriod)
+            scanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        } else {
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            requestCode -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    scanBleDevice()
+                } else {
+                    Toast.makeText(this, "Permissions denied, no ble-scanning possible!", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
 }
