e03771ad1e2f4e0fad33b557da500c39fc98c974
JannisMeyer <80952891+JannisMeyer@users.noreply.github.com>
2023-10-20 15:09:19 +0200

    added button and bluetooth/ble permissions

--- app/src/main/java/com/example/bleapp/MainActivity.kt
+++ app/src/main/java/com/example/bleapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.bleapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.bleapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //connect this activity with corresponding display
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //set OnClickListener for BLE-Button
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "BLE!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
